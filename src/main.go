91b64999a05bc58a00c686f775ec0fa53b6638d1
chr1swill <alexanderwilliams20003@gmail.com>
2025-09-05 04:19:14 -0400

    feat: workout type stuff closer to db for simiplicity

--- src/main.go
+++ src/main.go
@@ -29,9 +29,10 @@ func check_rollback(err *error, tx *sql.Tx) {
 	}
 }
 
-var ADJUSTABLE_WEIGHT_SET_VALUES = []float32{ 0.0, 5.0, 8.3, 9.2, 12.5,
-			11.5, 15, 16, 19.3, 18.5, 22.5, 27.5, 32,
-			34, 38.5, 40.5, 45}
+var ADJUSTABLE_WEIGHT_SET_VALUES = []float32{
+	0.0, 5.0, 8.3, 9.2, 12.5, 11.5, 15, 16, 19.3,
+	18.5, 22.5, 27.5, 32, 34, 38.5, 40.5, 45};
+		
 func main() {
 	var err error;
 	var db *sql.DB;
@@ -192,9 +193,7 @@ func main() {
 			return
 		}
 
-		data.Weights = []float32{ 0.0, 5.0, 8.3, 9.2, 12.5,
-			11.5, 15, 16, 19.3, 18.5, 22.5, 27.5, 32,
-			34, 38.5, 40.5, 45};
+		data.Weights = ADJUSTABLE_WEIGHT_SET_VALUES;
 
 		err = tmpl.ExecuteTemplate(w, "index.html", data);
 		if err != nil {
